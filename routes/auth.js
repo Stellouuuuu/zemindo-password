95f744995eba9d3bd3a1e826205a4e1f8c07e211
Stellouuuuu <stella.gbaguidi@epitech.eu>
2025-08-06 18:04:53 +0100

    add some modification

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { requestReset, confirmCode, subscribe, renvoilien, sendwelcome} = require('../controllers/authController');
+const { requestReset, confirmCode, subscribe, renvoilien, sendWelcome} = require('../controllers/authController');
 
 router.post('/reset-password', requestReset);
 router.post('/confirm-code', confirmCode);
 router.post('/subscribe', subscribe);
 router.post('/renvoilien', renvoilien);
-router.post('/sendwelcome', sendwelcome);
+router.post('/sendwelcome', sendWelcome);
 
 module.exports = router;
