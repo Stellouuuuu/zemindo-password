eaafcd0e87921ab3973b816baa1c73e7c2e07787
Stellouuuuu <stella.gbaguidi@epitech.eu>
2025-08-06 17:52:29 +0100

    add some modification

--- controllers/authController.js
+++ controllers/authController.js
@@ -109,7 +109,7 @@ exports.renvoilien = async (req, res) => {
   }
 };
 
-exports.welcome = async (req, res) => {
+exports.sendwelcome = async (req, res) => {
   const { email } = req.body;
   if (!email) {
     return res.status(400).json({ message: "Champs invalides" });
